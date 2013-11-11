4004e25f69b7fadd6249569c9df19624b4db0e26
AhnSeongHyun <sh84.ahn@gmail.com>
2013-11-12 08:52:58 +0900

    [Ã߰¡] keys Ã߰¡ ¹× °¢ API º° ±⺻ Ŭ·¡½º Ã߰¡

---
+++ DaumOpenAPI/contents/contents.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+
+import sys
+sys.path.append("../common/")
+
+from request import *
+
+
+'''
+Created on 2013. 11. 2.
+
+@author: seonghyunan
+'''
+
+
+class contents(object):
+    pass
---
+++ DaumOpenAPI/search/search.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*-
+
+import sys
+sys.path.append("../common/")
+
+from request import *
+
+
+'''
+Created on 2013. 11. 2.
+
+@author: seonghyunan
+'''
+class search(object):
+    pass
