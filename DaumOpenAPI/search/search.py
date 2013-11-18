c5d3e60a675e822ecf801b425348850331f9f625
AhnSeongHyun <sh84.ahn@gmail.com>
2013-11-19 08:56:30 +0900

    [add] search api, blog, board etc..

--- DaumOpenAPI/search/search.py
+++ DaumOpenAPI/search/search.py
@@ -12,4 +12,107 @@ Created on 2013. 11. 2.
 @author: seonghyunan
 '''
 class search(object):
-    pass
+    
+    base_url = "http://apis.daum.net/search"
+    apikey = None
+    
+    def __init__(self, apikey):
+        if apikey is None:
+            print "apikey is None."
+        else:
+            self.apikey = apikey
+            
+    
+    def blog(self, q=None,  result=10, pageno=1, sort='date', output='xml'):
+        params={}
+        params['q'] = q
+        params['result'] = result
+        params['pageno']= pageno
+        params['sort'] = sort
+        params['output'] = output
+        params['apikey'] = self.apikey
+        return  request(self.base_url+"/blog", params=params)
+    
+    def board(self, q=None,  result=10, pageno=1, sort='accu', output='json'):
+        params={}
+        params['q'] = q
+        params['result'] = result
+        params['pageno']= pageno
+        params['sort'] = sort
+        params['output'] = output
+        params['apikey'] = self.apikey
+        return  request(self.base_url+"/board", params=params)
+    
+    
+    def web(self, q=None,  result=10, pageno=1, output='xml'):
+        params={}
+        params['q'] = q
+        params['result'] = result
+        params['pageno']= pageno
+        params['sort'] = sort
+        params['output'] = output
+        return  request(self.base_url+"/web", params=params)
+    
+    def image(self, q=None,  result=10, pageno=1, sort='accu', output='json'):
+        params={}
+        params['q'] = q
+        params['result'] = result
+        params['pageno']= pageno
+        params['sort'] = sort
+        params['output'] = output
+        params['apikey'] = self.apikey
+        return  request(self.base_url+"/image", params=params)
+    
+    
+    def cafe(self, q=None,  result=10, pageno=1, sort='date', output='xml'):
+        params={}
+        params['q'] = q
+        params['result'] = result
+        params['pageno']= pageno
+        params['sort'] = sort
+        params['output'] = output
+        params['apikey'] = self.apikey
+        return  request(self.base_url+"/cafe", params=params)
+    
+    def video(self, q=None,  result=10, pageno=1, sort='accuracy', output='json'):
+        params={}
+        params['q'] = q
+        params['result'] = result
+        params['pageno']= pageno
+        params['sort'] = sort
+        params['output'] = output
+        params['apikey'] = self.apikey
+        return  request(self.base_url+"/vclip", params=params)
+    
+    def knowledge(self, q=None,  result=10, pageno=1, sort='accuracy', range='all', output='xml'):
+        params={}
+        params['q'] = q
+        params['result'] = result
+        params['pageno']= pageno
+        params['sort'] = sort
+        params['range'] = range
+        params['output'] = output
+        params['apikey'] = self.apikey
+        return  request(self.base_url+"/knowledge", params=params)
+    
+    
+    def book(self, q=None,  target='meta', cate_id=None, result=10, pageno=1, sort='popular', searchType='all', output='xml'):
+        params={}
+        params['q'] = q
+        
+        if cate_id is not None:
+            params['cate_id'] = cate_id;
+             
+        params['result'] = result
+        params['pageno']= pageno
+        params['sort'] = sort
+        params['range'] = range
+        params['searchType'] = searchType
+        params['apikey'] = self.apikey
+        return  request(self.base_url+"/book", params=params)
+    
+    
+
+if __name__ == "__main__":
+    pass 
+     
