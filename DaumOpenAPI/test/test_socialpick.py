6c901fe83e7325e79fe25255e8cfe3f628419098
AhnSeongHyun <sh84.ahn@gmail.com>
2013-12-12 16:36:32 +0900

    [add] testcase fix

--- DaumOpenAPI/test/test_socialpick.py
+++ DaumOpenAPI/test/test_socialpick.py
@@ -16,44 +16,27 @@ class TestSocialPickFunctions(unittest.TestCase):
         self.sp = socialpick("a4c8c716d0d6c2d652cede75a6c688dba9d0164c")
 
     def test_category_c(self):
-        rc = self.sp.search(10,'c', 'json')
+        rc = self.sp.search(5,'c', 'json')
+        
         self.assertEqual(int(rc.socialpick.item[0].rank), 1);
         self.assertEqual(int(rc.socialpick.item[1].rank), 2);
         self.assertEqual(int(rc.socialpick.item[2].rank), 3);
-        self.assertEqual(int(rc.socialpick.item[3].rank), 4);
-        self.assertEqual(int(rc.socialpick.item[4].rank), 5);
-        self.assertEqual(int(rc.socialpick.item[5].rank), 6);
-        self.assertEqual(int(rc.socialpick.item[6].rank), 7);
-        self.assertEqual(int(rc.socialpick.item[7].rank), 8);
-        self.assertEqual(int(rc.socialpick.item[8].rank), 9);
-        self.assertEqual(int(rc.socialpick.item[9].rank), 10);
-        
+        self.assertEqual(int(rc.socialpick.item[3].rank), 4); 
     
     def test_category_s(self):
-        rc = self.sp.search(10,'s', 'json')
+        rc = self.sp.search(5,'s', 'json')
+        
         self.assertEqual(int(rc.socialpick.item[0].rank), 1);
         self.assertEqual(int(rc.socialpick.item[1].rank), 2);
         self.assertEqual(int(rc.socialpick.item[2].rank), 3);
-        self.assertEqual(int(rc.socialpick.item[3].rank), 4);
-        self.assertEqual(int(rc.socialpick.item[4].rank), 5);
-        self.assertEqual(int(rc.socialpick.item[5].rank), 6);
-        self.assertEqual(int(rc.socialpick.item[6].rank), 7);
-        self.assertEqual(int(rc.socialpick.item[7].rank), 8);
-        self.assertEqual(int(rc.socialpick.item[8].rank), 9);
-        self.assertEqual(int(rc.socialpick.item[9].rank), 10);
+        self.assertEqual(int(rc.socialpick.item[3].rank), 4); 
     
     def test_category_e(self):
-        rc = self.sp.search(10,'e', 'json')
+        rc = self.sp.search(5,'e', 'json')
         self.assertEqual(int(rc.socialpick.item[0].rank), 1);
         self.assertEqual(int(rc.socialpick.item[1].rank), 2);
         self.assertEqual(int(rc.socialpick.item[2].rank), 3);
-        self.assertEqual(int(rc.socialpick.item[3].rank), 4);
-        self.assertEqual(int(rc.socialpick.item[4].rank), 5);
-        self.assertEqual(int(rc.socialpick.item[5].rank), 6);
-        self.assertEqual(int(rc.socialpick.item[6].rank), 7);
-        self.assertEqual(int(rc.socialpick.item[7].rank), 8);
-        self.assertEqual(int(rc.socialpick.item[8].rank), 9);
-        self.assertEqual(int(rc.socialpick.item[9].rank), 10);
+        self.assertEqual(int(rc.socialpick.item[3].rank), 4); 
     
  
 if __name__ == '__main__':
