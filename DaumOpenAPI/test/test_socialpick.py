4728f5b3a16ec3640aaa10a769c6cae6ccc310ca
AhnSeongHyun <sh84.ahn@gmail.com>
2013-12-12 16:30:59 +0900

    [add] testcase for socialpick

--- DaumOpenAPI/test/test_socialpick.py
+++ DaumOpenAPI/test/test_socialpick.py
@@ -4,27 +4,58 @@ import random
 import unittest
 
 import sys
-sys.path.append("../local/")
+sys.path.append("../socialpick/")
 sys.path.append("../result/")
 
-from local import local
+from socialpick import socialpick
 
-class TestLocalApiFunctions(unittest.TestCase):
+class TestSocialPickFunctions(unittest.TestCase):
 
-    lc = None
+    sp = None
     def setUp(self):
-        self.lc = local("48408559bb754dd16c517a5fd6d01e376a4be151")
+        self.sp = socialpick("a4c8c716d0d6c2d652cede75a6c688dba9d0164c")
 
-    def test_addr2coord(self):
-        print self.lc.addr2coord(u"¥Î«–∑Œ", 1,"json")
+    def test_category_c(self):
+        rc = self.sp.search(10,'c', 'json')
+        self.assertEqual(int(rc.socialpick.item[0].rank), 1);
+        self.assertEqual(int(rc.socialpick.item[1].rank), 2);
+        self.assertEqual(int(rc.socialpick.item[2].rank), 3);
+        self.assertEqual(int(rc.socialpick.item[3].rank), 4);
+        self.assertEqual(int(rc.socialpick.item[4].rank), 5);
+        self.assertEqual(int(rc.socialpick.item[5].rank), 6);
+        self.assertEqual(int(rc.socialpick.item[6].rank), 7);
+        self.assertEqual(int(rc.socialpick.item[7].rank), 8);
+        self.assertEqual(int(rc.socialpick.item[8].rank), 9);
+        self.assertEqual(int(rc.socialpick.item[9].rank), 10);
         
-    def test_coord2addr(self):
-        print self.lc.coord2addr('37.507502379027','127.05590291409', 'WGS84','simple',"json")
-        
-    def test_transcoord(self):
-        print self.lc.transcoord('37.507502379027','127.05590291409', 'WGS84','TM',"json")
-        
-          
+    
+    def test_category_s(self):
+        rc = self.sp.search(10,'s', 'json')
+        self.assertEqual(int(rc.socialpick.item[0].rank), 1);
+        self.assertEqual(int(rc.socialpick.item[1].rank), 2);
+        self.assertEqual(int(rc.socialpick.item[2].rank), 3);
+        self.assertEqual(int(rc.socialpick.item[3].rank), 4);
+        self.assertEqual(int(rc.socialpick.item[4].rank), 5);
+        self.assertEqual(int(rc.socialpick.item[5].rank), 6);
+        self.assertEqual(int(rc.socialpick.item[6].rank), 7);
+        self.assertEqual(int(rc.socialpick.item[7].rank), 8);
+        self.assertEqual(int(rc.socialpick.item[8].rank), 9);
+        self.assertEqual(int(rc.socialpick.item[9].rank), 10);
+    
+    def test_category_e(self):
+        rc = self.sp.search(10,'e', 'json')
+        self.assertEqual(int(rc.socialpick.item[0].rank), 1);
+        self.assertEqual(int(rc.socialpick.item[1].rank), 2);
+        self.assertEqual(int(rc.socialpick.item[2].rank), 3);
+        self.assertEqual(int(rc.socialpick.item[3].rank), 4);
+        self.assertEqual(int(rc.socialpick.item[4].rank), 5);
+        self.assertEqual(int(rc.socialpick.item[5].rank), 6);
+        self.assertEqual(int(rc.socialpick.item[6].rank), 7);
+        self.assertEqual(int(rc.socialpick.item[7].rank), 8);
+        self.assertEqual(int(rc.socialpick.item[8].rank), 9);
+        self.assertEqual(int(rc.socialpick.item[9].rank), 10);
+    
+ 
 if __name__ == '__main__':
-    suite = unittest.TestLoader().loadTestsFromTestCase(TestLocalApiFunctions)
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestSocialPickApiFunctions)
     unittest.TextTestRunner(verbosity=2).run(suite)
