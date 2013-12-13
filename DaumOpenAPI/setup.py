68d08412e0297a1607ad86e0caf9f57f6bbf7d84
AhnSeongHyun <sh84.ahn@gmail.com>
2013-12-13 09:43:55 +0900

    [add] ready for pip uploading

---
+++ DaumOpenAPI/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='daum_openapi',
+
+      version=version,
+      description="A module of parsing the open graph protocol and extracing the summary of webpage.",
+      long_description=open("README.md").read() + "\n",
+      classifiers=[
+      'Development Status :: 3 - Alpha',
+      'Intended Audience :: Developers',
+      'Programming Language :: Python',
+      'Topic :: Text Processing :: Markup :: HTML',
+      'Topic :: Software Development :: Libraries :: Python Modules',
+      ], 
+      keywords='daum openapi',
+      author='SeongHyun.Ahn',
+      author_email='sh84.ahn@gmail.com',
+      url='https://github.com/AhnSeongHyun/daum_openapi',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'xmltodict',
+          'requests'
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      
+      )
---
+++ DaumOpenAPI/socialpick/__init__.py
@@ -0,0 +1,1 @@
+from socialpick import *
