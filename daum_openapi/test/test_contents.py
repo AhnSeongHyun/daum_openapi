9677f0dc7d259c225f77dd00ee328dc5aa0f1338
AhnSeongHyun <sh84.ahn@gmail.com>
2013-12-16 17:39:44 +0900

    [testcode] for travis

--- daum_openapi/test/test_contents.py
+++ daum_openapi/test/test_contents.py
@@ -13,19 +13,23 @@ class TestContentsApiFunctions(unittest.TestCase):
     ct = None
     def setUp(self):
         self.ct = contents("b3e0456c1bf9facc191106ca35f9871141cb435a")
+        print "setup"
 
     def test_contents_movie_json(self):
         rc = self.ct.movie(u"공범", 10, 1,"json")
+        print "test_contents_movie_json"
         self.assertEqual(rc.channel.q, u'공범')
         
         
     def test_contents_movie_xml(self):
         rc = self.ct.movie(u"공범", 10, 1,"xml")
+        print "test_contents_movie_xml"
         self.assertEqual(rc.channel.q, u'공범')
         
         
     def test_contents_movie_pagecount_zero(self):
         rc = self.ct.movie(u"공범", 10, 0,"xml")
+        print "test_contents_movie_pagecount_zero"
         self.assertEqual(rc.apierror.dcode, u'111')
         
 
